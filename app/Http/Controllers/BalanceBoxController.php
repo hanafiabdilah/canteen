d5675b93f7faf99e1b19c6076231b24f1cfa4268
hanafiabdilah <handilah81@gmail.com>
2022-07-06 06:20:58 +0700

    UPDATE: saldo

--- app/Http/Controllers/BalanceBoxController.php
+++ app/Http/Controllers/BalanceBoxController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\ProductSold;
 use App\Models\Student;
 use Illuminate\Http\Request;
@@ -9,21 +10,12 @@ use Illuminate\Http\Request;
 class BalanceBoxController extends Controller
 {
     public function index(){
-        $total_saldo = 0;
-        $saldo = 0;
-        $products = ProductSold::orderBy('created_at', 'DESC')->get();
         $students = Student::orderBy('saldo', 'DESC')->get();
-
-        foreach($products as $product){
-            $total_saldo = $total_saldo + $product->detail->price;
-        }
-
-        foreach($students as $student){
-            $saldo = $saldo + $student->saldo;
-        }
-
+        
+        $total_saldo = Product::where('sold', true)->sum('price');
+        $saldo = $students->sum('saldo');
         $saldo_ditarik = $total_saldo - $saldo;
 
-        return view('pages.balancebox', compact('products', 'students', 'total_saldo', 'saldo', 'saldo_ditarik'));
+        return view('pages.balancebox', compact('students', 'total_saldo', 'saldo', 'saldo_ditarik'));
     }
 }
