76425cea1076a05b95bf42412af41c79db3941df
hanafiabdilah <handilah81@gmail.com>
2022-07-05 18:35:13 +0700

    ADD: migration and model

--- database/migrations/2022_07_05_113135_create_product_sold_table.php
+++ database/migrations/2022_07_05_113135_create_product_sold_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateProductSoldTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('product_sold', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id');
+            $table->bigInteger('buyer_id');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('product_sold');
     }
 }
